90f57583d205f8e9e4052b0ddca3012508ac3f5f
nikita.ziubin <nikita.ziubin@gmail.com>
2023-09-19 21:17:20 +0300

    Open google map using google api key

---
+++ WebApplication3/wwwroot/js/GoogleMap.js
@@ -0,0 +1,9 @@
+﻿function initMap()
+{
+    var opt = {
+        center: { lat: 39.285394, lng: 20.341723 },
+        zoom = 4
+    }
+    var map = new google.maps.map(document.getElementById("map"), opt);
+    return 0;
+}
