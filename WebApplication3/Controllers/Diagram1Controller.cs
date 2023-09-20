dd0e18da48f14b68113793cb74e346c1ff437eec
nikita.ziubin <nikita.ziubin@gmail.com>
2023-09-20 17:12:47 +0300

    Add two statistics diagrams

---
+++ WebApplication3/Controllers/Diagram1Controller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WEBLab1.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Diagram1Controller : ControllerBase
+	{
+		private readonly HotelContext _context;
+		public Diagram1Controller(HotelContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet("JsonData")]
+		public JsonResult JsonData()
+		{
+			//var categories = _context.Matches.Include(c => c.Goals).ToList();
+			var hotelContext = _context.Bookings.Include(p => p.roomNavigation).ToList();
+			var rooms = _context.Rooms.ToList();
+			List<object> catBook = new List<object>();
+			catBook.Add(new[] { "Room number", "price" });
+
+			foreach (var c in rooms)
+			{
+				if (c.state == false)
+				{
+					catBook.Add(new object[] { c.roomNumber.ToString(), c.oneNightPrice});
+				}
+			}
+
+			return new JsonResult(catBook);
+		}
+	}
+}
---
+++ WebApplication3/Controllers/Diagram2Controller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApplication3.Controllers
+{
+	//[Authorize(Roles = "admin, user")]
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Diagram2Controller : ControllerBase
+	{
+		private readonly HotelContext _context;
+		public Diagram2Controller(HotelContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet("JsonData")]
+		public JsonResult JsonData()
+		{
+			//var categories = _context.Matches.Include(c => c.Goals).ToList();
+			var hotelContext = _context.Bookings.Include(p => p.roomNavigation).ToList();
+			var rooms = _context.Bookings.ToList();
+			List<object> catBook = new List<object>();
+			catBook.Add(new[] { "Room number", "Departure date" });
+
+			foreach (var c in rooms)
+			{
+				if (c.roomNavigation.state == true)
+				{
+					catBook.Add(new object[] { c.departureDate, c.roomNavigation.roomNumber });
+				}
+			}
+
+			return new JsonResult(catBook);
+		}
+	}
+}
